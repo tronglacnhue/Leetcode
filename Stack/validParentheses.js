37116cc9f9224965b5c9163c7807334093a65216
tronglacnhue <tronglacnhue@gmail.com>
2024-05-09 20:51:19 +0700

    solve validParenthese using stack

--- Stack/validParentheses.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    //it's just a easy problem, we just use stack to contain the characters, if stack.peek is suitable with s[i] (we loop s) we just pop it out, otherwise we put s[i] in the top of stack
-    //if stack is empty after loop, it's valid, otherwise it's not.
-    let st = new stack()
-};
