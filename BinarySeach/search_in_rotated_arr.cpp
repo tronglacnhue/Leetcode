61c5e33eef7c402f9f7abfb1da1a22b96ccbe945
tronglacnhue <tronglacnhue@gmail.com>
2024-05-16 13:42:46 +0700

    Solve search in rotated array by using binary search

--- BinarySeach/search_in_rotated_arr.cpp
+++ BinarySeach/search_in_rotated_arr.cpp
@@ -44,15 +44,15 @@ int search(vector<int>& nums, int target) {
             if (nums[mid] == target) {
                 return mid;
             }
-            if(nums[low] < nums[high]) {
-                if(nums[mid] > target) {
+            if(nums[low] < nums[mid]) {
+                if(nums[mid] >= target && target >= nums[low]) {
                     high = mid - 1;
                 } else low = mid + 1;
             } else {
-                if(nums[low] < target && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
+                if(nums[mid] <= target && target <= nums[high]) {
                     low = mid + 1;
+                } else {
+                    high = mid - 1;
                 }
             }
         }
